8edffd8256a8b752ea335712e8582bedead47f5a
Dmitriy Goraschenko <dgoraschenko@gmail.com>
2023-09-16 12:12:32 +0200

    added forwarding and tunneling

--- goproxystarter.go
+++ goproxystarter.go
@@ -14,19 +14,23 @@ import (
 	"strings"
 	"time"
 
+	tls "github.com/refraction-networking/utls"
+
 	"github.com/cloudveiltech/goproxy"
 	"github.com/inconshreveable/go-vhost"
 	icap "github.com/patriciy/icap-client"
 )
 
 type Config struct {
-	icapServerReqUrl  string
-	icapServerRespUrl string
-	port              uint16
+	icapServerReqUrl    string
+	icapServerRespUrl   string
+	proxyPort           uint16
+	tunnelPort          uint16
+	forwardProxyAddress string
 }
 
 var (
-	config = Config{"", "", 12344}
+	config = Config{"", "", 12344, 12000, ""}
 	proxy  *goproxy.ProxyHttpServer
 	server *http.Server
 )
@@ -80,138 +84,12 @@ func (dumb dumbResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
 	return dumb, bufio.NewReadWriter(bufio.NewReader(dumb), bufio.NewWriter(dumb)), nil
 }
 
-func runHttpsListener() {
-	// listen to the TLS ClientHello but make it a CONNECT request instead
-	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", config.port))
-
-	if err != nil {
-		log.Printf("Error listening for https connections - %v", err)
-		return
-	}
-
-	for {
-		c, err := ln.Accept()
-		if err != nil {
-			log.Printf("Error accepting new connection - %v", err)
-			continue
-		}
-
-		go func(c net.Conn) {
-			tlsConn, err := vhost.TLS(c)
-			localPort := tlsConn.RemoteAddr().(*net.TCPAddr).Port
-
-			port := 443
-			ipString := "127.0.0.1"
-			remoteAddr := tlsConn.LocalAddr().String()
-			if strings.Count(remoteAddr, ":") > 1 {
-				//ipv6
-				ipString = "::1"
-			}
-
-			host := tlsConn.Host()
-			if host == "" {
-				host = ipString
-			}
-
-			if err != nil {
-				log.Printf("Assuming plain http connection - %v", err)
-				chainReqToHttp(tlsConn)
-				return
-			}
-
-			if proxy.Verbose {
-				log.Printf("Reading dest port for %d is %d", localPort, port)
-			}
-
-			if proxy.Verbose {
-				log.Printf("Https handler called for %s:%s", host, strconv.Itoa(port))
-			}
-
-			host = net.JoinHostPort(host, strconv.Itoa(port))
-			resp := dumbResponseWriter{tlsConn}
-			connectReq := &http.Request{
-				Method: "CONNECT",
-				URL: &url.URL{
-					Opaque: host,
-					Host:   host,
-				},
-				Host:   host,
-				Header: make(http.Header),
-			}
-
-			proxy.ServeHTTP(resp, connectReq)
-		}(c)
-	}
-}
-
-func startHttpServer() *http.Server {
-	srv := &http.Server{Addr: fmt.Sprintf(":%d", config.port+1)}
-	srv.Handler = proxy
-
-	// go func() {
-	// 	http.ListenAndServe(":6060", nil)
-	// }()
-	go func() {
-		if err := srv.ListenAndServe(); err != nil {
-			// cannot panic, because this probably is an intentional close
-			log.Printf("Httpserver: ListenAndServe() error: %s", err)
-			server = nil
-		}
-	}()
-
-	// returning reference so caller can call Shutdown()
-	return srv
-}
-
-func chainReqToHttp(client net.Conn) {
-	localAddress := client.LocalAddr().(*net.TCPAddr).IP
-
-	remote, err := net.Dial("tcp", net.JoinHostPort(localAddress.String(), strconv.Itoa(int(config.port+1))))
-	if err != nil {
-		log.Printf("chainReqToHttp error connect %s", err)
-		return
-	}
-
-	defer remote.Close()
-	defer client.Close()
-
-	go func() {
-		nonBlockingCopy(remote, client)
-	}()
-
-	nonBlockingCopy(client, remote)
-}
-
-func nonBlockingCopy(from, to net.Conn) {
-	buf := make([]byte, 10240)
-	for {
-		from.SetDeadline(time.Now().Add(time.Minute * 10))
-		if server == nil {
-			log.Printf("Break chain on server stop")
-			break
-		}
-
-		n, err := from.Read(buf)
-		if err != nil && err != io.EOF {
-			log.Printf("error request %s", err)
-			break
-		}
-		if n == 0 {
-			break
-		}
-
-		if _, err := to.Write(buf[:n]); err != nil {
-			log.Printf("error response %s", err)
-			break
-		}
-
-	}
-}
-
-func startGoProxyServer(certPath, certKeyPath, icapServerReqUrl, icapServerRespUrl string, port uint16) {
+func startGoProxyServer(certPath, certKeyPath, icapServerReqUrl, icapServerRespUrl string, proxyPort, tunnelPort uint16, forwardProxyAddress string) {
 	config.icapServerReqUrl = icapServerReqUrl
 	config.icapServerRespUrl = icapServerRespUrl
-	config.port = port
+	config.proxyPort = proxyPort
+	config.tunnelPort = tunnelPort
+	config.forwardProxyAddress = forwardProxyAddress
 
 	//icap.SetDebugMode(true)
 	initGoProxy()
@@ -295,8 +173,18 @@ func startGoProxyServer(certPath, certKeyPath, icapServerReqUrl, icapServerRespU
 		})
 
 	go runHttpsListener()
+	go runTlsTunnelListener()
 
-	log.Printf("Server started on port %d", config.port)
+	log.Printf("Server started on port %d", config.proxyPort)
+	log.Printf("Tunnel listener started on port %d", config.tunnelPort)
+	if config.forwardProxyAddress != "" {
+		proxy.Tr = &http.Transport{Proxy: func(req *http.Request) (*url.URL, error) {
+			return url.Parse("http://" + config.forwardProxyAddress)
+		}}
+		proxy.ConnectDial = proxy.NewConnectDialToProxy("http://" + config.forwardProxyAddress)
+
+		log.Printf("Forwarding traffic through %s", config.forwardProxyAddress)
+	}
 }
 
 func initIcapServerConnection() (*icap.Response, *icap.Response) {
@@ -335,5 +223,156 @@ func stopGoProxyServer() {
 		context, _ := context.WithTimeout(context.Background(), 1*time.Second)
 		server.Shutdown(context)
 		server = nil
+	}
+}
+
+func runTlsTunnelListener() {
+	// listen to the TLS ClientHello but make it a CONNECT request instead
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", config.tunnelPort))
+
+	if err != nil {
+		log.Printf("Error listening for https connections - %v", err)
+		return
+	}
+
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			log.Printf("Error accepting new connection - %v", err)
+			continue
+		}
+
+		go func(c net.Conn) {
+			tlsConn := tls.Server(c, &tls.Config{Certificates: []tls.Certificate{goproxy.GoproxyCa}})
+			chainReqToHttp(tlsConn, config.proxyPort)
+		}(c)
+	}
+}
+
+func runHttpsListener() {
+	// listen to the TLS ClientHello but make it a CONNECT request instead
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", config.proxyPort))
+
+	if err != nil {
+		log.Printf("Error listening for https connections - %v", err)
+		return
+	}
+
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			log.Printf("Error accepting new connection - %v", err)
+			continue
+		}
+
+		go func(c net.Conn) {
+			tlsConn, err := vhost.TLS(c)
+			localPort := tlsConn.RemoteAddr().(*net.TCPAddr).Port
+
+			port := 443
+			ipString := "127.0.0.1"
+			remoteAddr := tlsConn.LocalAddr().String()
+			if strings.Count(remoteAddr, ":") > 1 {
+				//ipv6
+				ipString = "::1"
+			}
+
+			host := tlsConn.Host()
+			if host == "" {
+				host = ipString
+			}
+
+			if err != nil {
+				log.Printf("Assuming plain http connection - %v", err)
+				chainReqToHttp(tlsConn, config.proxyPort+1)
+				return
+			}
+
+			if proxy.Verbose {
+				log.Printf("Reading dest port for %d is %d", localPort, port)
+			}
+
+			if proxy.Verbose {
+				log.Printf("Https handler called for %s:%s", host, strconv.Itoa(port))
+			}
+
+			host = net.JoinHostPort(host, strconv.Itoa(port))
+			resp := dumbResponseWriter{tlsConn}
+			connectReq := &http.Request{
+				Method: "CONNECT",
+				URL: &url.URL{
+					Opaque: host,
+					Host:   host,
+				},
+				Host:   host,
+				Header: make(http.Header),
+			}
+
+			proxy.ServeHTTP(resp, connectReq)
+		}(c)
+	}
+}
+
+func startHttpServer() *http.Server {
+	srv := &http.Server{Addr: fmt.Sprintf(":%d", config.proxyPort+1)}
+	srv.Handler = proxy
+
+	// go func() {
+	// 	http.ListenAndServe(":6060", nil)
+	// }()
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			// cannot panic, because this probably is an intentional close
+			log.Printf("Httpserver: ListenAndServe() error: %s", err)
+			server = nil
+		}
+	}()
+
+	// returning reference so caller can call Shutdown()
+	return srv
+}
+
+func chainReqToHttp(client net.Conn, port uint16) {
+	localAddress := client.LocalAddr().(*net.TCPAddr).IP
+
+	remote, err := net.Dial("tcp", net.JoinHostPort(localAddress.String(), strconv.Itoa(int(port))))
+	if err != nil {
+		log.Printf("chainReqToHttp error connect %s", err)
+		return
+	}
+
+	defer remote.Close()
+	defer client.Close()
+
+	go func() {
+		nonBlockingCopy(remote, client)
+	}()
+
+	nonBlockingCopy(client, remote)
+}
+
+func nonBlockingCopy(from, to net.Conn) {
+	buf := make([]byte, 10240)
+	for {
+		from.SetDeadline(time.Now().Add(time.Minute * 10))
+		if server == nil {
+			log.Printf("Break chain on server stop")
+			break
+		}
+
+		n, err := from.Read(buf)
+		if err != nil && err != io.EOF {
+			log.Printf("error request %s", err)
+			break
+		}
+		if n == 0 {
+			break
+		}
+
+		if _, err := to.Write(buf[:n]); err != nil {
+			log.Printf("error response %s", err)
+			break
+		}
+
 	}
 }
